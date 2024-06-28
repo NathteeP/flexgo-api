f41ec20f94da5772903e6160317e581f6d545d30
NathteeP <whatisitz.plus@gmail.com>
2024-06-28 16:53:11 +0700

    finish seed setup

--- prisma/resetDB.js
+++ prisma/resetDB.js
@@ -1,10 +1,21 @@
 require("dotenv").config()
 const { PrismaClient } = require("@prisma/client")
+const { execSync } = require("child_process")
+const seeding = require("./seed")
 const prisma = new PrismaClient()
 
 async function run() {
-    await prisma.$executeRawUnsafe("DROP DATABASE flexgo_group_project")
-    await prisma.$executeRawUnsafe("CREATE DATABAE flexgo_group_project")
+    try{
+        await prisma.$executeRawUnsafe("DROP DATABASE IF EXISTS flexgo_group_project")
+        await prisma.$executeRawUnsafe("CREATE DATABASE flexgo_group_project")
+    
+        execSync("pnpm prisma db push", { stdio: "inherit" });
+    
+        await seeding()
+    } catch (err) {
+        console.log(err)
+    }
+
 }
 
 console.log("Reset DB...")
